75e15ae826ae96646a1a4356e2dda47a1f659657
victma <victormalet@gmail.com>
2018-01-30 12:30:52 +0100

    Add account info jsp and servlet

---
+++ src/java/fr/ensta/authenticator/AccountServlet.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fr.ensta.authenticator;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author eleve
+ */
+public class AccountServlet extends HttpServlet {
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            RequestDispatcher dispatcher;
+            ServletContext context = getServletContext();
+            
+            request.setAttribute("firstname", "Toto");
+            
+            dispatcher = context.getNamedDispatcher("accountInfo");
+            dispatcher.forward(request, response);
+        } catch (Exception e) {
+            log("Exception dans AccountServlet.doGet()", e);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Not implemented</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>POST requests not implemeneted (yet)</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Manage client account";
+    }
+
+}
---
+++ web/AccountInfo.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : accountInfo
+    Created on : Jan 30, 2018, 12:24:23 PM
+    Author     : eleve
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Account Info</title>
+    </head>
+    <body>
+        <h1>Hello <%=request.getAttribute("firstname")%>!</h1>
+    </body>
+</html>
