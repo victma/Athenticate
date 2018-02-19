38b77047b958b94bc577092dbebb0ee365f2d004
Eleve <eleve@localhost.localdomain>
2018-02-19 18:52:54 +0100

    Throw in LDAP authenticate

--- src/java/fr/ensta/authenticator/Ldap.java
+++ src/java/fr/ensta/authenticator/Ldap.java
@@ -18,23 +18,19 @@ import com.unboundid.ldap.sdk.SimpleBindRequest;
  */
 public class Ldap {
 
-    public static LDAPConnection authenticate(String username, String password){
-        
-        
+    public static LDAPConnection authenticate(String username, String password) throws LDAPException {
         LDAPConnection ldap = null;
-        try {
-            ldap = new LDAPConnection("localhost.localdomain", 1389);
-        
-            SearchResult sr = ldap.search("ou=People,dc=ensta,dc=fr", SearchScope.SUB, "(uid=" + username + ")");
-       
-            if (sr.getEntryCount() == 0) {
-                System.out.println("KO");
-            } else {
-                String dn = sr.getSearchEntries().get(0).getDN();
-                ldap = new LDAPConnection("localhost.localdomain", 1389, dn, password);
-                System.out.print("connected \n");
-            }
-        } catch (LDAPException ex) { System.out.print(ex.getMessage() + "\n");}
+        ldap = new LDAPConnection("localhost.localdomain", 1389);
+
+        SearchResult sr = ldap.search("ou=People,dc=ensta,dc=fr", SearchScope.SUB, "(uid=" + username + ")");
+
+        if (sr.getEntryCount() == 0) {
+            System.out.println("KO");
+        } else {
+            String dn = sr.getSearchEntries().get(0).getDN();
+            ldap = new LDAPConnection("localhost.localdomain", 1389, dn, password);
+            System.out.print("connected \n");
+        }
         
         return ldap;
     }
@@ -46,9 +42,7 @@ public class Ldap {
         if (request == null){ return -1;}
         String dn = request.getBindDN();
         
-        try {
-            
-            
+        try {   
             LDAPResult result=ldap.modify(dn,mod);
         } catch (LDAPException ex) {
             System.out.print(ex.getMessage());
